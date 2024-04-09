1b43b544247eaaa8463a79b5360a8c30f017f816
Gustavohps10 <gustavoh.santos735@gmail.com>
2024-04-09 14:49:24 -0300

    add - cadastro de usuarios

--- src/cadastro/backup/ucadusuario.pas
+++ src/cadastro/backup/ucadusuario.pas
@@ -5,13 +5,23 @@ unit uCadUsuario;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ZDataset, uCadModelo;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, DBCtrls, StdCtrls,
+  ZDataset, uCadModelo, DB, dm;
 
 type
 
   { TCadUsuarioF }
 
   TCadUsuarioF = class(TCadModeloF)
+    edtUsuarioId: TDBEdit;
+    edtUsuarioId1: TDBEdit;
+    edtUsuarioId2: TDBEdit;
+    edtUsuarioId3: TDBEdit;
+    lblUsuarioId: TLabel;
+    lblNomeCompleto: TLabel;
+    lblUsuario: TLabel;
+    lblSenha: TLabel;
+    procedure qryCadNewRecord(DataSet: TDataSet);
   private
 
   public
@@ -24,6 +34,21 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TCadUsuarioF }
+
+procedure TCadUsuarioF.qryCadNewRecord(DataSet: TDataSet);
+begin
+  with dmF.qryGenerica do
+   begin
+    Close;
+    SQL.Clear;
+    SQL.Add('select nextval('+ QuotedStr('usuario_id')+') AS CODIGO');
+    Open;
+    qryCad.FieldByName('id').asInteger := FieldByName('CODIGO').AsInteger;
+   end;
+  pgcPrincipal.ActivePage := tbCadastro;
+end;
 
 end.
 
